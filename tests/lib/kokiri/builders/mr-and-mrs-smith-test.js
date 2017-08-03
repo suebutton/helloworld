625e7245814babed77c59ff6fc0bebcd3d259e61
Will Myers <will@usebutton.com>
2017-08-03 17:44:08 -0400

    Add MrAndMrsSmith builder (#25)
    * Add MrAndMrsSmith builder
    
    * Cleanup
    
    * Rebase updates

---
+++ tests/lib/kokiri/builders/mr-and-mrs-smith-test.js
@@ -0,0 +1,101 @@
+const assert = require('assert');
+
+const KokiriConfig = require('../../../../lib/kokiri/kokiri-config');
+
+describe('lib/kokiri/builders/mr-and-mrs-smith', function() {
+  beforeEach(function() {
+    const approvals = [
+      {
+        status: 'approved',
+        audience: 'org-XXX',
+        organization: 'org-4a2d4ae6a222295d',
+      },
+    ];
+
+    this.config = new KokiriConfig([], [], [], [], [], approvals);
+
+    this.builder = this.config.createBuilder('org-XXX', 'org-4a2d4ae6a222295d');
+  });
+
+  describe('#appAction', function() {
+    it('returns an app action', function() {
+      assert.deepEqual(this.builder.appAction({}, 'ios', 'srctok-XXX'), {
+        app_link: 'mrandmrssmith://?btn_ref=srctok-XXX',
+        browser_link: 'https://www.mrandmrssmith.com?btn_ref=srctok-XXX',
+      });
+
+      assert.deepEqual(this.builder.appAction({}, 'android', 'srctok-XXX'), {
+        app_link: null,
+        browser_link: 'https://www.mrandmrssmith.com?btn_ref=srctok-XXX',
+      });
+    });
+
+    it('returns an app action with destination', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          {
+            pathname: '/bloop',
+            query: {},
+            hash: null,
+          },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: null,
+          browser_link:
+            'https://www.mrandmrssmith.com/bloop?btn_ref=srctok-XXX',
+        }
+      );
+    });
+  });
+
+  describe('#universalLink', function() {
+    it('returns a universal link', function() {
+      assert.deepEqual(
+        this.builder.universalLink({}, 'srctok-XXX'),
+        'https://track.bttn.io/mrandmrssmith?btn_ref=srctok-XXX'
+      );
+    });
+
+    it('returns a universal link for static affiliation', function() {
+      assert.deepEqual(
+        this.builder.universalLink({}),
+        'https://track.bttn.io/mrandmrssmith?btn_ref=org-XXX'
+      );
+    });
+
+    it('returns a universal link with destination', function() {
+      assert.deepEqual(
+        this.builder.universalLink(
+          {
+            pathname: '/bloop',
+            query: {},
+            hash: null,
+          },
+          'srctok-XXX'
+        ),
+        'https://track.bttn.io/mrandmrssmith/bloop?btn_ref=srctok-XXX'
+      );
+    });
+  });
+
+  it('returns a destination from a url', function() {
+    assert.deepEqual(
+      this.builder.destinationFromUrl(
+        'https://mrandmrssmith.com/bloop?utm_campaign=BEST%20OIL'
+      ),
+      {
+        pathname: '/bloop',
+        query: { utm_campaign: 'BEST OIL' },
+        hash: null,
+      }
+    );
+
+    assert.deepEqual(this.builder.destinationFromUrl(''), {
+      pathname: null,
+      query: {},
+      hash: null,
+    });
+  });
+});
