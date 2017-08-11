de7b8fd974058ed138494e0a6bb4b297d1e6a1c9
Jiaqi Liu <jiaqi216@users.noreply.github.com>
2017-08-11 11:41:49 -0400

    ENG-231 link support for CJ (#42)
    * Logic for CommissionJunction links
    
    * Create tests for Commission Junction link builder
    
    * Include missing test file
    
    * remove url null tests

---
+++ tests/lib/kokiri/builders/commission-junction-test.js
@@ -0,0 +1,128 @@
+const assert = require('assert');
+
+const KokiriConfig = require('../../../../lib/kokiri/kokiri-config');
+
+describe('lib/kokiri/builders/commission-junction,', function() {
+  beforeEach(function() {
+    const approvals = [
+      {
+        status: 'approved',
+        audience: 'org-XXX',
+        organization: 'org-0b394cbf5a4d9aa8',
+      },
+    ];
+
+    this.config = new KokiriConfig([], [], [], [], [], approvals);
+
+    this.builder = this.config.createBuilder('org-XXX', 'org-0b394cbf5a4d9aa8');
+  });
+
+  describe('#appAction', function() {
+    it('returns an app action with destination', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          {
+            url: 'https://drizly_cj.com',
+          },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: null,
+          browser_link:
+            'http://www.dpbolvw.net/click-8395017-12515534?sid=srctok-XXX&url=https%3A%2F%2Fdrizly.com%2F&btn_ref=srctok-XXX',
+        }
+      );
+
+      assert.deepEqual(
+        this.builder.appAction(
+          {
+            url: 'https://drizly_cj.com',
+          },
+          'android',
+          'srctok-XXX'
+        ),
+        {
+          app_link: null,
+          browser_link:
+            'http://www.dpbolvw.net/click-8395017-12515534?sid=srctok-XXX&url=https%3A%2F%2Fdrizly.com%2F&btn_ref=srctok-XXX',
+        }
+      );
+    });
+
+    it('returns an app action with destination and a path', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          {
+            url: 'https://drizly_cj.com/a/b/c?foo=bar#123',
+          },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: null,
+          browser_link:
+            'http://www.dpbolvw.net/click-8395017-12515534?sid=srctok-XXX&url=https%3A%2F%2Fdrizly.com%2Fa%2Fb%2Fc&btn_ref=srctok-XXX',
+        }
+      );
+    });
+
+    it('returns app action with null values if there is no org_id to mid mapping', function() {
+      this.builder.merchantId = 'org-XXX';
+      assert.deepEqual(
+        this.builder.appAction(
+          {
+            url: 'http://www.example.com',
+          },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: null,
+          browser_link: null,
+        }
+      );
+      assert.deepEqual(
+        this.builder.appAction(
+          {
+            url: 'http://www.example.com',
+          },
+          'android',
+          'srctok-XXX'
+        ),
+        {
+          app_link: null,
+          browser_link: null,
+        }
+      );
+    });
+  });
+  describe('destinationFromUrl', function() {
+    it('returns a destination from a url', function() {
+      assert.deepEqual(
+        this.builder.destinationFromUrl('https://www.techarmor.com/iphone-7'),
+        {
+          url: 'https://www.techarmor.com/iphone-7',
+        }
+      );
+
+      assert.deepEqual(this.builder.destinationFromUrl(''), {
+        url: '',
+      });
+    });
+  });
+  describe('#universalLink', function() {
+    it('universal link returns null', function() {
+      assert.deepEqual(
+        this.builder.universalLink(
+          {
+            url: 'https://www.techarmor.com/iphone-7',
+          },
+          'ios',
+          'srctok-XXX'
+        ),
+        null
+      );
+    });
+  });
+});
