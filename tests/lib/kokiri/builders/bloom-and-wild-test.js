f08d6c111de65bb892e7aba7cbe0abac0539e1fd
Will Myers <will@usebutton.com>
2017-11-16 12:39:24 -0500

    PUB-303 Add Bloom and Wild (#107)
    * Add Bloom and Wild builder
    
    * Add Bloom and Wild web links
    
    * Update docstring
    
    * Add Quidco redirect id
    
    * Fixup app mappings
    
    * Update test description

---
+++ tests/lib/kokiri/builders/bloom-and-wild-test.js
@@ -0,0 +1,66 @@
+const assert = require('assert');
+
+const KokiriConfig = require('../../../../lib/kokiri/kokiri-config');
+
+describe('lib/kokiri/builders/bloom-and-wild', function() {
+  beforeEach(function() {
+    const approvals = [
+      {
+        status: 'approved',
+        audience: 'org-XXX',
+        organization: 'org-2705011eae8616ea',
+      },
+    ];
+
+    this.config = new KokiriConfig([], [], [], [], { approvals });
+
+    this.builder = this.config.createBuilder('org-XXX', 'org-2705011eae8616ea');
+  });
+
+  describe('#appAction', function() {
+    it('returns an app action', function() {
+      assert.deepEqual(this.builder.appAction({}, 'ios', 'srctok-XXX'), {
+        app_link: 'bloom://?btn_ref=srctok-XXX',
+        browser_link: 'https://www.bloomandwild.com?btn_ref=srctok-XXX',
+      });
+    });
+
+    it('return an app action for non-app-supported pathnames', function() {
+      assert.deepEqual(
+        this.builder.appAction({ pathname: '/bleep' }, 'ios', 'srctok-XXX'),
+        {
+          app_link: null,
+          browser_link: 'https://www.bloomandwild.com/bleep?btn_ref=srctok-XXX',
+        }
+      );
+    });
+  });
+
+  describe('#webAction', function() {
+    it('returns a web action', function() {
+      assert.deepEqual(this.builder.webAction({}, 'ios', 'srctok-XXX'), {
+        app_link: 'https://bloomandwild.bttn.io?btn_ref=srctok-XXX',
+        browser_link: 'https://www.bloomandwild.com?btn_ref=srctok-XXX',
+      });
+    });
+  });
+
+  it('returns a destination from a url', function() {
+    assert.deepEqual(
+      this.builder.destinationFromUrl(
+        'https://www.bloomandwild.com/items/p1297?utm_campaign=BEST%20OIL'
+      ),
+      {
+        pathname: '/items/p1297',
+        query: { utm_campaign: 'BEST OIL' },
+        hash: null,
+      }
+    );
+
+    assert.deepEqual(this.builder.destinationFromUrl(''), {
+      pathname: null,
+      query: {},
+      hash: null,
+    });
+  });
+});
