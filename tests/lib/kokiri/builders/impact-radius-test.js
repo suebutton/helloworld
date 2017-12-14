fc078cbe9bed38853e2c1fc35071a3affc944da5
Will Myers <will@usebutton.com>
2017-12-14 11:18:27 -0500

    Add arbitraty content routing for IR links (#126)

--- tests/lib/kokiri/builders/impact-radius-test.js
+++ tests/lib/kokiri/builders/impact-radius-test.js
@@ -45,66 +45,66 @@ describe('lib/kokiri/builders/impact-radius', function() {
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-24621b367f4280bc')
-          .appAction({}, 'ios', 'srctok-XXX'),
+          .appAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://goto.target.com/c/415484/81938/2092?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://goto.target.com/c/415484/81938/2092?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
         }
       );
 
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-2ef55bcceba936bf')
-          .appAction({}, 'ios', 'srctok-XXX'),
+          .appAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://kohls.sjv.io/c/415484/362118/5349?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://kohls.sjv.io/c/415484/362118/5349?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
         }
       );
 
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-4c5c4337f5359c9f')
-          .appAction({}, 'ios', 'srctok-XXX'),
+          .appAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://janecom.7eer.net/c/415484/136645/2703?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://janecom.7eer.net/c/415484/136645/2703?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
         }
       );
 
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-3bec3b5c0cac44ad')
-          .appAction({}, 'ios', 'srctok-XXX'),
+          .appAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://backcountry.pxf.io/c/415484/358742/5311?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://backcountry.pxf.io/c/415484/358742/5311?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
         }
       );
 
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-03418dec42db44bc')
-          .appAction({}, 'ios', 'srctok-XXX'),
+          .appAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://hpn.houzz.com/c/415484/372747/5454?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://hpn.houzz.com/c/415484/372747/5454?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
         }
       );
 
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-7829938c0c640b81')
-          .appAction({}, 'ios', 'srctok-XXX'),
+          .appAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://partners.hotwire.com/c/415484/205226/3435?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://partners.hotwire.com/c/415484/205226/3435?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
         }
       );
     });
@@ -115,66 +115,88 @@ describe('lib/kokiri/builders/impact-radius', function() {
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-24621b367f4280bc')
-          .webAction({}, 'ios', 'srctok-XXX'),
+          .webAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://goto.target.com/c/415484/81938/2092?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://goto.target.com/c/415484/81938/2092?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
         }
       );
 
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-2ef55bcceba936bf')
-          .webAction({}, 'ios', 'srctok-XXX'),
+          .webAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://kohls.sjv.io/c/415484/362118/5349?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://kohls.sjv.io/c/415484/362118/5349?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
         }
       );
 
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-4c5c4337f5359c9f')
-          .webAction({}, 'ios', 'srctok-XXX'),
+          .webAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://janecom.7eer.net/c/415484/136645/2703?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://janecom.7eer.net/c/415484/136645/2703?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
         }
       );
 
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-3bec3b5c0cac44ad')
-          .webAction({}, 'ios', 'srctok-XXX'),
+          .webAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://backcountry.pxf.io/c/415484/358742/5311?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://backcountry.pxf.io/c/415484/358742/5311?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
         }
       );
 
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-03418dec42db44bc')
-          .webAction({}, 'ios', 'srctok-XXX'),
+          .webAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://hpn.houzz.com/c/415484/372747/5454?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://hpn.houzz.com/c/415484/372747/5454?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
         }
       );
 
       assert.deepEqual(
         this.config
           .createBuilder('org-XXX', 'org-7829938c0c640b81')
-          .webAction({}, 'ios', 'srctok-XXX'),
+          .webAction({ url: 'https://merchant.com' }, 'ios', 'srctok-XXX'),
         {
           app_link: null,
           browser_link:
-            'http://partners.hotwire.com/c/415484/205226/3435?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&btn_ref=srctok-XXX',
+            'http://partners.hotwire.com/c/415484/205226/3435?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fmerchant.com&btn_ref=srctok-XXX',
+        }
+      );
+    });
+  });
+
+  describe('destinationFromUrl', function() {
+    it('returns a destination from a url', function() {
+      assert.deepEqual(
+        this.config
+          .createBuilder('org-XXX', 'org-7829938c0c640b81')
+          .destinationFromUrl('https://www.target.com/iphone-7'),
+        {
+          url: 'https://www.target.com/iphone-7',
+        }
+      );
+
+      assert.deepEqual(
+        this.config
+          .createBuilder('org-XXX', 'org-7829938c0c640b81')
+          .destinationFromUrl(''),
+        {
+          url: '',
         }
       );
     });
