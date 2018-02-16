6ed2612a20d51c45c7239cc58d65e31b0a6e93c1
Sam Propis <30512361+spropis@users.noreply.github.com>
2018-02-16 17:12:01 -0500

    updated etsy builder to reference new par-pars (utmcontent and utmcampaign), as well as etsy test file (#152)

--- tests/lib/kokiri/builders/etsy-test.js
+++ tests/lib/kokiri/builders/etsy-test.js
@@ -2,17 +2,89 @@ const assert = require('assert');
 
 const KokiriConfig = require('../../../../lib/kokiri/kokiri-config');
 
+const ETSY_ORG_ID = 'org-3ee55c6f49b96819';
+const IBOTTA_ORG_ID = 'org-2d432a88b9bb8bda';
+const SHOPKICK_ORG_ID = 'org-030575eddb72b4df';
+const ACORNS_ORG_ID = 'org-0ae7f03a3ad7f664';
+
 describe('lib/kokiri/builders/etsy', function() {
   beforeEach(function() {
     const approvals = [
       {
         status: 'approved',
         audience: 'org-XXX',
-        organization: 'org-3ee55c6f49b96819',
+        organization: ETSY_ORG_ID,
+      },
+      {
+        status: 'approved',
+        audience: IBOTTA_ORG_ID,
+        organization: ETSY_ORG_ID,
+      },
+      {
+        status: 'approved',
+        audience: SHOPKICK_ORG_ID,
+        organization: ETSY_ORG_ID,
+      },
+      {
+        status: 'approved',
+        audience: ACORNS_ORG_ID,
+        organization: ETSY_ORG_ID,
       },
     ];
 
-    this.config = new KokiriConfig([], [], [], [], { approvals });
+    const partnerParameters = [
+      {
+        id: '12345',
+        organization: ETSY_ORG_ID,
+        default_value: 'us_location_buyer',
+        name: 'utmcampaign',
+      },
+      {
+        id: '54321',
+        organization: ETSY_ORG_ID,
+        default_value: 'ibotta',
+        name: 'utmcontent',
+      },
+    ];
+
+    const partnerValues = [
+      {
+        partner_parameter: '12345',
+        organization: IBOTTA_ORG_ID,
+        value: 'us_location_buyer',
+      },
+      {
+        partner_parameter: '54321',
+        organization: IBOTTA_ORG_ID,
+        value: 'ibotta',
+      },
+      {
+        partner_parameter: '12345',
+        organization: SHOPKICK_ORG_ID,
+        value: 'us_location_buyer',
+      },
+      {
+        partner_parameter: '54321',
+        organization: SHOPKICK_ORG_ID,
+        value: 'shopkick',
+      },
+      {
+        partner_parameter: '12345',
+        organization: ACORNS_ORG_ID,
+        value: 'us_location_buyer',
+      },
+      {
+        partner_parameter: '54321',
+        organization: ACORNS_ORG_ID,
+        value: 'acorns',
+      },
+    ];
+
+    this.config = new KokiriConfig([], [], [], [], {
+      approvals,
+      partnerParameters,
+      partnerValues,
+    });
 
     this.builder = this.config.createBuilder('org-XXX', 'org-3ee55c6f49b96819');
   });
@@ -21,16 +93,49 @@ describe('lib/kokiri/builders/etsy', function() {
     it('returns an app action', function() {
       assert.deepEqual(this.builder.appAction({}, 'ios', 'srctok-XXX'), {
         app_link:
-          'etsy://?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+          'etsy://?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         browser_link:
-          'https://www.etsy.com?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+          'https://www.etsy.com?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
       });
 
       assert.deepEqual(this.builder.appAction({}, 'android', 'srctok-XXX'), {
         app_link:
-          'etsy://home?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+          'etsy://home?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         browser_link:
-          'https://www.etsy.com?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+          'https://www.etsy.com?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
+      });
+    });
+
+    it('returns an app action overriding affiliation parameters', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          {
+            query: {
+              utm_medium: 'pavel',
+              utm_source: 'pavel',
+              utm_campaign: 'pavel',
+              utm_content: 'pavel',
+            },
+          },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link:
+            'etsy://?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
+          browser_link:
+            'https://www.etsy.com?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
+        }
+      );
+    });
+
+    it('returns an app action with per-publisher tokens', function() {
+      const builder = this.config.createBuilder(ACORNS_ORG_ID, ETSY_ORG_ID);
+      assert.deepEqual(builder.appAction({}, 'ios', 'srctok-XXX'), {
+        app_link:
+          'etsy://?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=acorns&btn_ref=srctok-XXX',
+        browser_link:
+          'https://www.etsy.com?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=acorns&btn_ref=srctok-XXX',
       });
     });
 
@@ -45,9 +150,9 @@ describe('lib/kokiri/builders/etsy', function() {
         ),
         {
           app_link:
-            'etsy://featured/halloween?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'etsy://featured/halloween?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
           browser_link:
-            'https://www.etsy.com/featured/halloween?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://www.etsy.com/featured/halloween?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         }
       );
 
@@ -61,9 +166,9 @@ describe('lib/kokiri/builders/etsy', function() {
         ),
         {
           app_link:
-            'etsy://featured/halloween?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'etsy://featured/halloween?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
           browser_link:
-            'https://www.etsy.com/featured/halloween?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://www.etsy.com/featured/halloween?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         }
       );
     });
@@ -79,9 +184,9 @@ describe('lib/kokiri/builders/etsy', function() {
         ),
         {
           app_link:
-            'etsy://listing/123?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'etsy://listing/123?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
           browser_link:
-            'https://www.etsy.com/listing/123?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://www.etsy.com/listing/123?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         }
       );
 
@@ -95,9 +200,9 @@ describe('lib/kokiri/builders/etsy', function() {
         ),
         {
           app_link:
-            'etsy://listing/123/slug?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'etsy://listing/123/slug?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
           browser_link:
-            'https://www.etsy.com/listing/123/slug?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://www.etsy.com/listing/123/slug?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         }
       );
 
@@ -111,9 +216,9 @@ describe('lib/kokiri/builders/etsy', function() {
         ),
         {
           app_link:
-            'etsy://listing/123?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'etsy://listing/123?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
           browser_link:
-            'https://www.etsy.com/listing/123?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://www.etsy.com/listing/123?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         }
       );
     });
@@ -129,9 +234,9 @@ describe('lib/kokiri/builders/etsy', function() {
         ),
         {
           app_link:
-            'etsy://c?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'etsy://c?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
           browser_link:
-            'https://www.etsy.com/c?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://www.etsy.com/c?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         }
       );
 
@@ -146,7 +251,7 @@ describe('lib/kokiri/builders/etsy', function() {
         {
           app_link: null,
           browser_link:
-            'https://www.etsy.com/c?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://www.etsy.com/c?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         }
       );
 
@@ -160,9 +265,9 @@ describe('lib/kokiri/builders/etsy', function() {
         ),
         {
           app_link:
-            'etsy://c/home/bedding/sheets?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'etsy://c/home/bedding/sheets?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
           browser_link:
-            'https://www.etsy.com/c/home/bedding/sheets?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://www.etsy.com/c/home/bedding/sheets?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         }
       );
 
@@ -177,7 +282,7 @@ describe('lib/kokiri/builders/etsy', function() {
         {
           app_link: null,
           browser_link:
-            'https://www.etsy.com/c/home/bedding/sheets?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://www.etsy.com/c/home/bedding/sheets?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         }
       );
     });
@@ -187,16 +292,16 @@ describe('lib/kokiri/builders/etsy', function() {
     it('returns a web action', function() {
       assert.deepEqual(this.builder.webAction({}, 'ios', 'srctok-XXX'), {
         app_link:
-          'https://etsy.bttn.io?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+          'https://etsy.bttn.io?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         browser_link:
-          'https://www.etsy.com?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+          'https://www.etsy.com?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
       });
 
       assert.deepEqual(this.builder.webAction({}, 'android', 'srctok-XXX'), {
         app_link:
-          'https://etsy.bttn.io?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_fallback_exp=web&btn_ref=srctok-XXX',
+          'https://etsy.bttn.io?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_fallback_exp=web&btn_ref=srctok-XXX',
         browser_link:
-          'https://www.etsy.com?utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+          'https://www.etsy.com?utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
       });
     });
 
@@ -209,9 +314,9 @@ describe('lib/kokiri/builders/etsy', function() {
         ),
         {
           app_link:
-            'https://etsy.bttn.io/bloop?a=2&utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://etsy.bttn.io/bloop?a=2&utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
           browser_link:
-            'https://www.etsy.com/bloop?a=2&utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://www.etsy.com/bloop?a=2&utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         }
       );
     });
@@ -225,9 +330,9 @@ describe('lib/kokiri/builders/etsy', function() {
         ),
         {
           app_link:
-            'https://etsy.bttn.io/bloop?a=2&utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_fallback_exp=web&btn_ref=srctok-XXX',
+            'https://etsy.bttn.io/bloop?a=2&utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_fallback_exp=web&btn_ref=srctok-XXX',
           browser_link:
-            'https://www.etsy.com/bloop?a=2&utm_medium=affiliate&utm_campaign=us_location_buyer&utm_content=button&btn_ref=srctok-XXX',
+            'https://www.etsy.com/bloop?a=2&utm_medium=affiliate&utm_source=button&utm_campaign=us_location_buyer&utm_content=ibotta&btn_ref=srctok-XXX',
         }
       );
     });
