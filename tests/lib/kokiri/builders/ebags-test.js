77f253e150ad5b4bcf667c4372ee5824e4230554
Sam Propis <30512361+spropis@users.noreply.github.com>
2018-02-20 20:27:16 -0500

    PEP-8105 - Creating eBags Par-pars (#153)
    * PEP-8105: updating eBags kokiri builder to include partner parameters
    * updated ebags-test.js to look at staging org

--- tests/lib/kokiri/builders/ebags-test.js
+++ tests/lib/kokiri/builders/ebags-test.js
@@ -2,27 +2,83 @@ const assert = require('assert');
 
 const KokiriConfig = require('../../../../lib/kokiri/kokiri-config');
 
+const EBAGS_ORG_ID = 'org-67cb320e6548dfb3';
+const IBOTTA_ORG_ID = 'org-2d432a88b9bb8bda';
+const SHOPKICK_ORG_ID = 'org-030575eddb72b4df';
+
 describe('lib/kokiri/builders/ebags', function() {
   beforeEach(function() {
     const approvals = [
       {
         status: 'approved',
         audience: 'org-XXX',
-        organization: 'org-67cb320e6548dfb3',
+        organization: EBAGS_ORG_ID,
+      },
+      {
+        status: 'approved',
+        audience: IBOTTA_ORG_ID,
+        organization: EBAGS_ORG_ID,
+      },
+      {
+        status: 'approved',
+        audience: SHOPKICK_ORG_ID,
+        organization: EBAGS_ORG_ID,
       },
     ];
 
-    this.config = new KokiriConfig([], [], [], [], { approvals });
+    const partnerParameters = [
+      {
+        id: '12345',
+        organization: EBAGS_ORG_ID,
+        default_value: 'ibotta',
+        name: 'utmcampaign',
+      },
+      {
+        id: '54321',
+        organization: EBAGS_ORG_ID,
+        default_value: 'BTNIB',
+        name: 'sourceid',
+      },
+    ];
 
-    this.builder = this.config.createBuilder('org-XXX', 'org-67cb320e6548dfb3');
+    const partnerValues = [
+      {
+        partner_parameter: '12345',
+        organization: IBOTTA_ORG_ID,
+        value: 'ibotta',
+      },
+      {
+        partner_parameter: '54321',
+        organization: IBOTTA_ORG_ID,
+        value: 'BTNIB',
+      },
+      {
+        partner_parameter: '12345',
+        organization: SHOPKICK_ORG_ID,
+        value: 'shopkick',
+      },
+      {
+        partner_parameter: '54321',
+        organization: SHOPKICK_ORG_ID,
+        value: 'BTNSK',
+      },
+    ];
+
+    this.config = new KokiriConfig([], [], [], [], {
+      approvals,
+      partnerParameters,
+      partnerValues,
+    });
+
+    this.builder = this.config.createBuilder('org-XXX', EBAGS_ORG_ID);
   });
 
   describe('#appAction', function() {
-    it('returns an app action', function() {
+    it('returns an app action for ios', function() {
       assert.deepEqual(this.builder.appAction({}, 'ios', 'srctok-XXX'), {
         app_link: null,
         browser_link:
-          'https://www.ebags.com?sourceid=BTNIB&btnpid=org-XXX&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
+          'https://www.ebags.com?sourceid=BTNIB&btnpid=org-XXX&utm_campaign=ibotta&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
       });
     });
 
@@ -30,7 +86,39 @@ describe('lib/kokiri/builders/ebags', function() {
       assert.deepEqual(this.builder.appAction({}, 'android', 'srctok-XXX'), {
         app_link: null,
         browser_link:
-          'https://www.ebags.com?sourceid=BTNIB&btnpid=org-XXX&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
+          'https://www.ebags.com?sourceid=BTNIB&btnpid=org-XXX&utm_campaign=ibotta&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
+      });
+    });
+
+    it('returns an app action overriding affiliation parameters', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          {
+            query: {
+              sourceid: 'pavel',
+              btnpid: 'pavel',
+              utm_campaign: 'pavel',
+              utm_source: 'pavel',
+              utm_medium: 'pavel',
+            },
+          },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: null,
+          browser_link:
+            'https://www.ebags.com?sourceid=BTNIB&btnpid=org-XXX&utm_campaign=ibotta&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
+        }
+      );
+    });
+
+    it('returns an app action with per-publisher tokens', function() {
+      const builder = this.config.createBuilder(SHOPKICK_ORG_ID, EBAGS_ORG_ID);
+      assert.deepEqual(builder.appAction({}, 'ios', 'srctok-XXX'), {
+        app_link: null,
+        browser_link:
+          'https://www.ebags.com?sourceid=BTNSK&btnpid=org-030575eddb72b4df&utm_campaign=shopkick&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
       });
     });
 
@@ -48,7 +136,7 @@ describe('lib/kokiri/builders/ebags', function() {
         {
           app_link: null,
           browser_link:
-            'https://www.ebags.com/item/p1297?a=2&sourceid=BTNIB&btnpid=org-XXX&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
+            'https://www.ebags.com/item/p1297?a=2&sourceid=BTNIB&btnpid=org-XXX&utm_campaign=ibotta&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
         }
       );
     });
@@ -59,7 +147,7 @@ describe('lib/kokiri/builders/ebags', function() {
       assert.deepEqual(this.builder.webAction({}, 'ios', 'srctok-XXX'), {
         app_link: null,
         browser_link:
-          'https://www.ebags.com?sourceid=BTNIB&btnpid=org-XXX&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
+          'https://www.ebags.com?sourceid=BTNIB&btnpid=org-XXX&utm_campaign=ibotta&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
       });
     });
 
@@ -73,7 +161,7 @@ describe('lib/kokiri/builders/ebags', function() {
         {
           app_link: null,
           browser_link:
-            'https://www.ebags.com/bloop?a=2&sourceid=BTNIB&btnpid=org-XXX&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
+            'https://www.ebags.com/bloop?a=2&sourceid=BTNIB&btnpid=org-XXX&utm_campaign=ibotta&utm_source=button&utm_medium=affiliate&btn_ref=srctok-XXX',
         }
       );
     });
