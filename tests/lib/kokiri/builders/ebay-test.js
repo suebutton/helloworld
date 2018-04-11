94567a73811972491079a4f4c69806558eb7908e
shwetaatbutton <36678206+shwetaatbutton@users.noreply.github.com>
2018-04-11 13:35:15 -0400

    Added a webviewOnlyPath set to the ebay kokiri builder to enable webv… (#171)
    * Added a webviewOnlyPath set to the ebay kokiri builder to enable webview only routing for /rpp/computers-networking and to support such requests in the future for other urls. Also added a appropriate test to the ebay-test file
    
    * Based on the PR comment, webviewOnlyPaths is not needed at this point for /rpp/computers-networking to only route to webview. That is taken care of with AppMappings.

--- tests/lib/kokiri/builders/ebay-test.js
+++ tests/lib/kokiri/builders/ebay-test.js
@@ -240,6 +240,24 @@ describe('lib/kokiri/builders/ebay', function() {
         }
       );
     });
+
+    it('returns no app action for web pages only links', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          {
+            hostname: 'www.ebay.com',
+            pathname: '/rpp/computers-networking',
+          },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: null,
+          browser_link:
+            'https://rover.ebay.com/rover/1/711-53200-19255-0/1?ff3=4&toolid=11800&pub=5575211063&campid=5337936547&customid=srctok-XXX&mpre=http%3A%2F%2Fwww.ebay.com%2Frpp%2Fcomputers-networking&btn_ref=srctok-XXX',
+        }
+      );
+    });
   });
 
   describe('#webAction', function() {
