b3b5bcf741786f7b8c5646bb28a464a785fe0e2a
Tyler Nappy <tylernappy@users.noreply.github.com>
2018-05-15 13:34:20 -0400

    Added Casper builder (#182)
    * Added Casper builder
    
    * Removed appactions for Casper
    
    * removed casper getPartnerSubdomain() function and AppMappings
    
    * Changed `app_link` in Casper test to `null`
    
    * removed matchHomepage variable from casper builder (unused)

---
+++ tests/lib/kokiri/builders/casper-test.js
@@ -0,0 +1,63 @@
+const assert = require('assert');
+
+const KokiriConfig = require('../../../../lib/kokiri/kokiri-config');
+
+describe('lib/kokiri/builders/casper', function() {
+  beforeEach(function() {
+    const approvals = [
+      {
+        status: 'approved',
+        audience: 'org-XXX',
+        organization: 'org-1baf990739ef9149',
+      },
+    ];
+
+    this.config = new KokiriConfig([], [], [], [], { approvals });
+
+    this.builder = this.config.createBuilder('org-XXX', 'org-1baf990739ef9149');
+  });
+
+  describe('#webAction', function() {
+    it('returns a web action', function() {
+      assert.deepEqual(this.builder.webAction({}, 'ios', 'srctok-XXX'), {
+        app_link: null,
+        browser_link: 'https://www.casper.com?btn_ref=srctok-XXX',
+      });
+    });
+
+    it('returns a web action with destination', function() {
+      assert.deepEqual(
+        this.builder.webAction(
+          { pathname: '/bloop', query: { a: 2 } },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: null,
+          browser_link: 'https://www.casper.com/bloop?a=2&btn_ref=srctok-XXX',
+        }
+      );
+    });
+  });
+
+  it('returns a destination from a url', function() {
+    assert.deepEqual(
+      this.builder.destinationFromUrl(
+        'https://www.casper.com/bloop?utm_campaign=BEST%20OIL'
+      ),
+      {
+        pathname: '/bloop',
+        query: {
+          utm_campaign: 'BEST OIL',
+        },
+        hash: null,
+      }
+    );
+
+    assert.deepEqual(this.builder.destinationFromUrl(''), {
+      pathname: null,
+      query: {},
+      hash: null,
+    });
+  });
+});
