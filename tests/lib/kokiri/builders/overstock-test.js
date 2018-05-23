cc8cf233f543951cbf5da5129e4df8e4b2693076
shwetaatbutton <36678206+shwetaatbutton@users.noreply.github.com>
2018-05-23 09:15:28 -0400

    Added support for btn_aff Par-Par in the overstock builder file and made necessary changes to the overstock test file. (#208)
    * Added support for btn_aff Par-Par in the overstock builder file and made necessary changes to the overstock test file.
    
    * Changed the variable name for parpar from BTN_AFF to BTN_AFF_NAME
    
    * Removed orgId constants that are not used in tests, added a test to verify passing specific publisher picks correct parpar value, added test to verify builder overrides incoming appaction affiliation parameters
    
    * Changed default value for btn_aff to Button. Corresponding changes are made to comstore as well as the tests. Also added btn_aff param to the override tests. Removed use of the merge function.

--- tests/lib/kokiri/builders/overstock-test.js
+++ tests/lib/kokiri/builders/overstock-test.js
@@ -2,46 +2,81 @@ const assert = require('assert');
 
 const KokiriConfig = require('../../../../lib/kokiri/kokiri-config');
 
+const OVERSTOCK_ORG_ID = 'org-695642d20753707a';
+const SHOPKICK_ORG_ID = 'org-030575eddb72b4df';
+
 describe('lib/kokiri/builders/overstock', function() {
   beforeEach(function() {
     const approvals = [
       {
         status: 'approved',
         audience: 'org-XXX',
-        organization: 'org-695642d20753707a',
+        organization: OVERSTOCK_ORG_ID,
+      },
+      {
+        status: 'approved',
+        audience: SHOPKICK_ORG_ID,
+        organization: OVERSTOCK_ORG_ID,
       },
     ];
 
-    this.config = new KokiriConfig([], [], [], [], { approvals });
+    const partnerParameters = [
+      {
+        id: '12345',
+        organization: OVERSTOCK_ORG_ID,
+        default_value: 'Button',
+        name: 'btn_aff',
+      },
+    ];
 
-    this.builder = this.config.createBuilder('org-XXX', 'org-695642d20753707a');
+    const partnerValues = [
+      {
+        partner_parameter: '12345',
+        organization: SHOPKICK_ORG_ID,
+        value: 'Shopkick',
+      },
+    ];
+
+    this.config = new KokiriConfig([], [], [], [], {
+      approvals,
+      partnerParameters,
+      partnerValues,
+    });
+
+    this.builder = this.config.createBuilder('org-XXX', OVERSTOCK_ORG_ID);
   });
 
   describe('#appAction', function() {
     it('returns an app action', function() {
       assert.deepEqual(this.builder.appAction({}, 'ios', 'srctok-XXX'), {
         app_link:
-          'ostk://www.overstock.com/home?PID=12345&AID=55555&affproviderId=4&CID=260521&btn_ref=srctok-XXX',
+          'ostk://www.overstock.com/home?PID=12345&AID=55555&affproviderId=4&CID=260521&btn_aff=Button&btn_ref=srctok-XXX',
         browser_link:
-          'https://www.overstock.com?siteId=4&SID=srctok-XXX&CID=260521&btn_ref=srctok-XXX',
+          'https://www.overstock.com?siteId=4&SID=srctok-XXX&CID=260521&btn_aff=Button&btn_ref=srctok-XXX',
       });
     });
 
     it('returns an app action for android', function() {
       assert.deepEqual(this.builder.appAction({}, 'android', 'srctok-XXX'), {
         app_link:
-          'ostk://www.overstock.com/home?PID=12345&AID=55555&affproviderId=4&CID=260521&btn_ref=srctok-XXX',
+          'ostk://www.overstock.com/home?PID=12345&AID=55555&affproviderId=4&CID=260521&btn_aff=Button&btn_ref=srctok-XXX',
         browser_link:
-          'https://www.overstock.com?siteId=4&SID=srctok-XXX&CID=260521&btn_ref=srctok-XXX',
+          'https://www.overstock.com?siteId=4&SID=srctok-XXX&CID=260521&btn_aff=Button&btn_ref=srctok-XXX',
       });
     });
 
-    it('returns a product app action with destination', function() {
+    it('returns empty app action for non home page requests and browser action overrides affliaition parameters', function() {
       assert.deepEqual(
         this.builder.appAction(
           {
             pathname: '/item/p1297',
-            query: { a: 2, siteId: 'pavel', SID: 'pavel', CID: 'pavel' },
+            query: {
+              a: 2,
+              siteId: 'pavel',
+              SID: 'pavel',
+              CID: 'pavel',
+              btn_aff: 'pavel',
+            },
             hash: null,
           },
           'ios',
@@ -50,9 +85,50 @@ describe('lib/kokiri/builders/overstock', function() {
         {
           app_link: null,
           browser_link:
-            'https://www.overstock.com/item/p1297?a=2&siteId=4&SID=srctok-XXX&CID=260521&btn_ref=srctok-XXX',
+            'https://www.overstock.com/item/p1297?a=2&siteId=4&SID=srctok-XXX&CID=260521&btn_aff=Button&btn_ref=srctok-XXX',
         }
       );
+    });
+
+    it('returns app action overriding affiliation parameters', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          {
+            query: {
+              a: 2,
+              siteId: 'pavel',
+              SID: 'pavel',
+              CID: 'pavel',
+              PID: 'pavel',
+              AID: 'pavel',
+              affproviderId: 'pavel',
+              btn_aff: 'pavel',
+            },
+            hash: null,
+          },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link:
+            'ostk://www.overstock.com/home?PID=12345&AID=55555&affproviderId=4&CID=260521&btn_aff=Button&btn_ref=srctok-XXX',
+          browser_link:
+            'https://www.overstock.com?a=2&siteId=4&SID=srctok-XXX&CID=260521&PID=pavel&AID=pavel&affproviderId=pavel&btn_aff=Button&btn_ref=srctok-XXX',
+        }
+      );
+    });
+
+    it('returns an app action with per-publisher tokens', function() {
+      const builder = this.config.createBuilder(
+        SHOPKICK_ORG_ID,
+        OVERSTOCK_ORG_ID
+      );
+      assert.deepEqual(builder.appAction({}, 'ios', 'srctok-XXX'), {
+        app_link:
+          'ostk://www.overstock.com/home?PID=12345&AID=55555&affproviderId=4&CID=260521&btn_aff=Shopkick&btn_ref=srctok-XXX',
+        browser_link:
+          'https://www.overstock.com?siteId=4&SID=srctok-XXX&CID=260521&btn_aff=Shopkick&btn_ref=srctok-XXX',
+      });
     });
   });
 
@@ -60,9 +136,9 @@ describe('lib/kokiri/builders/overstock', function() {
     it('returns a web action', function() {
       assert.deepEqual(this.builder.webAction({}, 'ios', 'srctok-XXX'), {
         app_link:
-          'https://overstock.bttn.io/home?PID=12345&AID=55555&affproviderId=4&CID=260521&btn_ref=srctok-XXX',
+          'https://overstock.bttn.io/home?PID=12345&AID=55555&affproviderId=4&CID=260521&btn_aff=Button&btn_ref=srctok-XXX',
         browser_link:
-          'https://www.overstock.com?siteId=4&SID=srctok-XXX&CID=260521&btn_ref=srctok-XXX',
+          'https://www.overstock.com?siteId=4&SID=srctok-XXX&CID=260521&btn_aff=Button&btn_ref=srctok-XXX',
       });
     });
 
@@ -76,7 +152,7 @@ describe('lib/kokiri/builders/overstock', function() {
         {
           app_link: null,
           browser_link:
-            'https://www.overstock.com/bloop?a=2&siteId=4&SID=srctok-XXX&CID=260521&btn_ref=srctok-XXX',
+            'https://www.overstock.com/bloop?a=2&siteId=4&SID=srctok-XXX&CID=260521&btn_aff=Button&btn_ref=srctok-XXX',
         }
       );
     });
