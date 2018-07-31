65f7893743296a6ba7216640dc9f235d914228d6
durandc <christopher.durand@gmail.com>
2018-07-31 13:15:53 -0400

    CD/PEP-13843 (#264)
    * added extended link support; removed support for android as there is no android app (PEP-13843)
    
    * Added example links for Warby Parker
    
    * Fixed to ensure single quotes are used for strings
    
    * re-adding single quotes without escape character
    
    * simplified universal link building with joinPathName()

--- tests/lib/kokiri/builders/warby-parker-test.js
+++ tests/lib/kokiri/builders/warby-parker-test.js
@@ -18,25 +18,71 @@ describe('lib/kokiri/builders/warby-parker', function() {
   });
 
   describe('#appAction', function() {
-    it('returns an app action', function() {
+    it('returns an app action on iOS', function() {
       assert.deepEqual(this.builder.appAction({}, 'ios', 'srctok-XXX'), {
-        app_link: 'wp://?btn_ref=srctok-XXX',
+        app_link: 'wp://app?btn_ref=srctok-XXX',
         browser_link: 'https://www.warbyparker.com?btn_ref=srctok-XXX',
       });
     });
 
-    it('returns an app action for android', function() {
+    it('returns a browser link only app action for android', function() {
       assert.deepEqual(this.builder.appAction({}, 'android', 'srctok-XXX'), {
-        app_link: 'wp://?btn_ref=srctok-XXX',
+        app_link: null,
         browser_link: 'https://www.warbyparker.com?btn_ref=srctok-XXX',
       });
+    });
+
+    it('returns an app action for the women sunglasses category on iOS', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          { pathname: '/sunglasses/women' },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: 'wp://app/sunglasses/women?btn_ref=srctok-XXX',
+          browser_link:
+            'https://www.warbyparker.com/sunglasses/women?btn_ref=srctok-XXX',
+        }
+      );
+    });
+
+    it('returns an app action for specific men eyeglasses on iOS', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          { pathname: '/eyeglasses/men/durand/deep-sea-blue-fade' },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link:
+            'wp://app/eyeglasses/men/durand/deep-sea-blue-fade?btn_ref=srctok-XXX',
+          browser_link:
+            'https://www.warbyparker.com/eyeglasses/men/durand/deep-sea-blue-fade?btn_ref=srctok-XXX',
+        }
+      );
+    });
+
+    it('returns a browser link only app action for the men eyeglasses category on android', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          { pathname: '/eyeglasses/men' },
+          'android',
+          'srctok-XXX'
+        ),
+        {
+          app_link: null,
+          browser_link:
+            'https://www.warbyparker.com/eyeglasses/men?btn_ref=srctok-XXX',
+        }
+      );
     });
 
     it('returns an app action for a non-supported app path', function() {
       assert.deepEqual(
         this.builder.appAction({ pathname: '/bloop' }, 'ios', 'srctok-XXX'),
         {
-          app_link: null,
+          app_link: 'wp://app/bloop?btn_ref=srctok-XXX',
           browser_link: 'https://www.warbyparker.com/bloop?btn_ref=srctok-XXX',
         }
       );
@@ -44,9 +90,9 @@ describe('lib/kokiri/builders/warby-parker', function() {
   });
 
   describe('#webAction', function() {
-    it('returns a web action', function() {
+    it('returns a web action on iOS', function() {
       assert.deepEqual(this.builder.webAction({}, 'ios', 'srctok-XXX'), {
-        app_link: 'https://warbyparker.bttn.io?btn_ref=srctok-XXX',
+        app_link: 'https://warbyparker.bttn.io/app?btn_ref=srctok-XXX',
         browser_link: 'https://www.warbyparker.com?btn_ref=srctok-XXX',
       });
     });
@@ -59,7 +105,8 @@ describe('lib/kokiri/builders/warby-parker', function() {
           'srctok-XXX'
         ),
         {
-          app_link: 'https://warbyparker.bttn.io/bloop?a=2&btn_ref=srctok-XXX',
+          app_link:
+            'https://warbyparker.bttn.io/app/bloop?a=2&btn_ref=srctok-XXX',
           browser_link:
             'https://www.warbyparker.com/bloop?a=2&btn_ref=srctok-XXX',
         }
