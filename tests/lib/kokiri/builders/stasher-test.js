40eb24f268216ac71d62940a89f330fd93420cd4
Colin Allen <colin@usebutton.com>
2018-09-07 13:45:51 -0400

    PEP-15058 - Builder for Stasher  (#286)
    * Added Kokiri builder for Stasher

---
+++ tests/lib/kokiri/builders/stasher-test.js
@@ -0,0 +1,63 @@
+const assert = require('assert');
+
+const KokiriConfig = require('../../../../lib/kokiri/kokiri-config');
+
+describe('lib/kokiri/builders/stasher', function() {
+  beforeEach(function() {
+    const approvals = [
+      {
+        status: 'approved',
+        audience: 'org-XXX',
+        organization: 'org-39656f81c033a5f8',
+      },
+    ];
+
+    this.config = new KokiriConfig([], [], [], [], { approvals });
+
+    this.builder = this.config.createBuilder('org-XXX', 'org-39656f81c033a5f8');
+  });
+
+  describe('#webAction', function() {
+    it('returns a web action', function() {
+      assert.deepEqual(this.builder.webAction({}, 'ios', 'srctok-XXX'), {
+        app_link: null,
+        browser_link: 'https://stasher.com?btn_ref=srctok-XXX',
+      });
+    });
+
+    it('returns a web action with destination', function() {
+      assert.deepEqual(
+        this.builder.webAction(
+          { pathname: '/bloop', query: { a: 2 } },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: null,
+          browser_link: 'https://stasher.com/bloop?a=2&btn_ref=srctok-XXX',
+        }
+      );
+    });
+  });
+
+  it('returns a destination from a url', function() {
+    assert.deepEqual(
+      this.builder.destinationFromUrl(
+        'https://stasher.com/bloop?utm_campaign=BEST%20OIL'
+      ),
+      {
+        pathname: '/bloop',
+        query: {
+          utm_campaign: 'BEST OIL',
+        },
+        hash: null,
+      }
+    );
+
+    assert.deepEqual(this.builder.destinationFromUrl(''), {
+      pathname: null,
+      query: {},
+      hash: null,
+    });
+  });
+});
