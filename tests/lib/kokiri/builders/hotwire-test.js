1ea00550e0cc5a4e4eca300b9592aae3aca74327
shwetaatbutton <36678206+shwetaatbutton@users.noreply.github.com>
2018-10-12 13:43:18 -0400

    Added new query params to Hotwire's app links (#325)
    * Added new query params to Hotwire's app links

--- tests/lib/kokiri/builders/hotwire-test.js
+++ tests/lib/kokiri/builders/hotwire-test.js
@@ -2,19 +2,48 @@ const assert = require('assert');
 
 const KokiriConfig = require('../../../../lib/kokiri/kokiri-config');
 
+const HOTWIRE_ORG_ID = 'org-7829938c0c640b81';
+const IBOTTA_ORG_ID = 'org-2d432a88b9bb8bda';
+
 describe('lib/kokiri/builders/hotwire', function() {
   beforeEach(function() {
     const approvals = [
       {
         status: 'approved',
         audience: 'org-XXX',
-        organization: 'org-7829938c0c640b81',
+        organization: HOTWIRE_ORG_ID,
+      },
+      {
+        status: 'approved',
+        audience: IBOTTA_ORG_ID,
+        organization: HOTWIRE_ORG_ID,
       },
     ];
 
-    this.config = new KokiriConfig([], [], [], [], { approvals });
+    const partnerParameters = [
+      {
+        id: '12345',
+        organization: HOTWIRE_ORG_ID,
+        default_value: '007',
+        name: 'siteid',
+      },
+    ];
 
-    this.builder = this.config.createBuilder('org-XXX', 'org-7829938c0c640b81');
+    const partnerValues = [
+      {
+        partner_parameter: '12345',
+        organization: IBOTTA_ORG_ID,
+        value: '100',
+      },
+    ];
+
+    this.config = new KokiriConfig([], [], [], [], {
+      approvals,
+      partnerParameters,
+      partnerValues,
+    });
+
+    this.builder = this.config.createBuilder('org-XXX', HOTWIRE_ORG_ID);
   });
 
   describe('#appAction', function() {
@@ -26,7 +55,7 @@ describe('lib/kokiri/builders/hotwire', function() {
           'srctok-XXX'
         ),
         {
-          app_link: 'hotwireapp://?btn_ref=srctok-XXX',
+          app_link: 'hotwireapp://?nwid=Bt&siteid=007&btn_ref=srctok-XXX',
           browser_link:
             'http://partners.hotwire.com/c/415484/205226/3435?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fwww.hotwire.com&btn_tkn=srctok-XXX',
         }
@@ -41,7 +70,7 @@ describe('lib/kokiri/builders/hotwire', function() {
           'srctok-XXX'
         ),
         {
-          app_link: 'hotwireapp://?btn_ref=srctok-XXX',
+          app_link: 'hotwireapp://?nwid=Bt&siteid=007&btn_ref=srctok-XXX',
           browser_link:
             'http://partners.hotwire.com/c/415484/205226/3435?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fwww.hotwire.com&btn_tkn=srctok-XXX',
         }
@@ -62,6 +91,39 @@ describe('lib/kokiri/builders/hotwire', function() {
         }
       );
     });
+
+    it('returns an app action overriding affiliation parameters', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          this.builder.getDestinationFromUrl(
+            'https://www.hotwire.com?siteid=doughnuts&nwid=apples'
+          ),
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: 'hotwireapp://?siteid=007&nwid=Bt&btn_ref=srctok-XXX',
+          browser_link:
+            'http://partners.hotwire.com/c/415484/205226/3435?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fwww.hotwire.com%3Fsiteid%3Ddoughnuts%26nwid%3Dapples&btn_tkn=srctok-XXX',
+        }
+      );
+    });
+
+    it('returns an app action with per-publisher tokens', function() {
+      const builder = this.config.createBuilder(IBOTTA_ORG_ID, HOTWIRE_ORG_ID);
+      assert.deepEqual(
+        builder.appAction(
+          { url: 'https://www.hotwire.com' },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: 'hotwireapp://?nwid=Bt&siteid=100&btn_ref=srctok-XXX',
+          browser_link:
+            'http://partners.hotwire.com/c/415484/205226/3435?subId1=srctok-XXX&subId2=org-2d432a88b9bb8bda&sharedid=org-2d432a88b9bb8bda&u=https%3A%2F%2Fwww.hotwire.com&btn_tkn=srctok-XXX',
+        }
+      );
+    });
   });
 
   describe('#webAction', function() {
@@ -73,7 +135,8 @@ describe('lib/kokiri/builders/hotwire', function() {
           'srctok-XXX'
         ),
         {
-          app_link: 'https://hotwire.bttn.io?btn_ref=srctok-XXX',
+          app_link:
+            'https://hotwire.bttn.io?nwid=Bt&siteid=007&btn_ref=srctok-XXX',
           browser_link:
             'http://partners.hotwire.com/c/415484/205226/3435?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fwww.hotwire.com&btn_tkn=srctok-XXX',
         }
@@ -88,7 +151,8 @@ describe('lib/kokiri/builders/hotwire', function() {
           'srctok-XXX'
         ),
         {
-          app_link: 'https://hotwire.bttn.io?btn_ref=srctok-XXX',
+          app_link:
+            'https://hotwire.bttn.io?nwid=Bt&siteid=007&btn_ref=srctok-XXX',
           browser_link:
             'http://partners.hotwire.com/c/415484/205226/3435?subId1=srctok-XXX&subId2=org-XXX&sharedid=org-XXX&u=https%3A%2F%2Fwww.hotwire.com&btn_tkn=srctok-XXX',
         }
