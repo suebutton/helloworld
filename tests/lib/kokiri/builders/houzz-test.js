367752ab9eec33de48c6132993a40a449d44220e
maryeoreilly <48571601+maryeoreilly@users.noreply.github.com>
2019-04-01 13:35:32 -0400

    PSUP-603: Add new par-par for Houzz (#469)
    Added new par-par "refid" for Houzz

--- tests/lib/kokiri/builders/houzz-test.js
+++ tests/lib/kokiri/builders/houzz-test.js
@@ -2,33 +2,64 @@ const assert = require('assert');
 
 const KokiriConfig = require('../../../../lib/kokiri/kokiri-config');
 
+const HOUZZ_ORG_ID = 'org-03418dec42db44bc';
+const IBOTTA_ORG_ID = 'org-2d432a88b9bb8bda';
+
 describe('lib/kokiri/builders/houzz', function() {
   beforeEach(function() {
     const approvals = [
       {
         status: 'approved',
         audience: 'org-XXX',
-        organization: 'org-03418dec42db44bc',
+        organization: HOUZZ_ORG_ID,
+      },
+      {
+        status: 'approved',
+        audience: IBOTTA_ORG_ID,
+        organization: HOUZZ_ORG_ID,
       },
     ];
 
-    this.config = new KokiriConfig([], [], [], [], { approvals });
+    const partnerParameters = [
+      {
+        id: '12',
+        organization: HOUZZ_ORG_ID,
+        default_value: 'us-ptr-mpl-btn-0000',
+        name: 'refid',
+      },
+    ];
 
-    this.builder = this.config.createBuilder('org-XXX', 'org-03418dec42db44bc');
+    const partnerValues = [
+      {
+        partner_parameter: '12',
+        organization: IBOTTA_ORG_ID,
+        value: 'us-ptr-mpl-btn-471159',
+      },
+    ];
+
+    this.config = new KokiriConfig([], [], [], [], {
+      approvals,
+      partnerParameters,
+      partnerValues,
+    });
+
+    this.builder = this.config.createBuilder('org-XXX', HOUZZ_ORG_ID);
   });
 
   describe('#appAction', function() {
     it('returns an app action', function() {
       assert.deepEqual(this.builder.appAction({}, 'ios', 'srctok-XXX'), {
-        app_link: 'houzz://?btn_ref=srctok-XXX',
-        browser_link: 'https://www.houzz.com?btn_ref=srctok-XXX',
+        app_link: 'houzz://?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
+        browser_link:
+          'https://www.houzz.com?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
       });
     });
 
     it('returns an app action for android', function() {
       assert.deepEqual(this.builder.appAction({}, 'android', 'srctok-XXX'), {
-        app_link: 'houzz://?btn_ref=srctok-XXX',
-        browser_link: 'https://www.houzz.com?btn_ref=srctok-XXX',
+        app_link: 'houzz://?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
+        browser_link:
+          'https://www.houzz.com?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
       });
     });
 
@@ -36,8 +67,10 @@ describe('lib/kokiri/builders/houzz', function() {
       assert.deepEqual(
         this.builder.appAction({ pathname: '/bloop' }, 'ios', 'srctok-XXX'),
         {
-          app_link: 'houzz:///bloop?btn_ref=srctok-XXX',
-          browser_link: 'https://www.houzz.com/bloop?btn_ref=srctok-XXX',
+          app_link:
+            'houzz:///bloop?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
+          browser_link:
+            'https://www.houzz.com/bloop?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
         }
       );
     });
@@ -50,9 +83,10 @@ describe('lib/kokiri/builders/houzz', function() {
           'srctok-XXX'
         ),
         {
-          app_link: 'houzz:///dogs-of-button?btn_ref=srctok-XXX',
+          app_link:
+            'houzz:///dogs-of-button?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
           browser_link:
-            'https://www.houzz.com/dogs-of-button?btn_ref=srctok-XXX',
+            'https://www.houzz.com/dogs-of-button?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
         }
       );
     });
@@ -65,10 +99,38 @@ describe('lib/kokiri/builders/houzz', function() {
           'srctok-XXX'
         ),
         {
-          app_link: 'houzz://?btn_ref=srctok-XXX',
-          browser_link: 'https://www.houzz.co.uk?btn_ref=srctok-XXX',
+          app_link: 'houzz://?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
+          browser_link:
+            'https://www.houzz.co.uk?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
         }
       );
+    });
+    it('returns an app action overriding affiliation parameters', function() {
+      assert.deepEqual(
+        this.builder.appAction(
+          {
+            query: {
+              refid: 'cats',
+            },
+          },
+          'ios',
+          'srctok-XXX'
+        ),
+        {
+          app_link: 'houzz://?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
+          browser_link:
+            'https://www.houzz.com?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
+        }
+      );
+    });
+
+    it('returns an app action with per-publisher tokens', function() {
+      const builder = this.config.createBuilder(IBOTTA_ORG_ID, HOUZZ_ORG_ID);
+      assert.deepEqual(builder.appAction({}, 'ios', 'srctok-XXX'), {
+        app_link: 'houzz://?refid=us-ptr-mpl-btn-471159&btn_ref=srctok-XXX',
+        browser_link:
+          'https://www.houzz.com?refid=us-ptr-mpl-btn-471159&btn_ref=srctok-XXX',
+      });
     });
   });
 
@@ -76,8 +138,9 @@ describe('lib/kokiri/builders/houzz', function() {
     it('returns a web action', function() {
       assert.deepEqual(this.builder.webAction({}, 'ios', 'srctok-XXX'), {
         app_link:
-          'https://houzz.bttn.io?btn_fallback_exp=web&btn_ref=srctok-XXX',
-        browser_link: 'https://www.houzz.com?btn_ref=srctok-XXX',
+          'https://houzz.bttn.io?refid=us-ptr-mpl-btn-0000&btn_fallback_exp=web&btn_ref=srctok-XXX',
+        browser_link:
+          'https://www.houzz.com?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
       });
     });
 
@@ -90,8 +153,9 @@ describe('lib/kokiri/builders/houzz', function() {
         ),
         {
           app_link:
-            'https://houzz.bttn.io?btn_fallback_exp=web&btn_ref=srctok-XXX',
-          browser_link: 'https://www.houzz.co.uk?btn_ref=srctok-XXX',
+            'https://houzz.bttn.io?refid=us-ptr-mpl-btn-0000&btn_fallback_exp=web&btn_ref=srctok-XXX',
+          browser_link:
+            'https://www.houzz.co.uk?refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
         }
       );
     });
@@ -105,8 +169,9 @@ describe('lib/kokiri/builders/houzz', function() {
         ),
         {
           app_link:
-            'https://houzz.bttn.io/bloop?a=2&btn_fallback_exp=web&btn_ref=srctok-XXX',
-          browser_link: 'https://www.houzz.com/bloop?a=2&btn_ref=srctok-XXX',
+            'https://houzz.bttn.io/bloop?a=2&refid=us-ptr-mpl-btn-0000&btn_fallback_exp=web&btn_ref=srctok-XXX',
+          browser_link:
+            'https://www.houzz.com/bloop?a=2&refid=us-ptr-mpl-btn-0000&btn_ref=srctok-XXX',
         }
       );
     });
